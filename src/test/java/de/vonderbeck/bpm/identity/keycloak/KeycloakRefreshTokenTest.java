9f1404af158db4c17e6db01da2fd8bd3bc0453d1
Gunnar von der Beck <Gunnar.von-der-Beck@accso.de>
2019-02-28 15:59:32 +0100

    Added test for refreshing an expired access token

---
+++ src/test/java/de/vonderbeck/bpm/identity/keycloak/KeycloakRefreshTokenTest.java
@@ -0,0 +1,33 @@
+package de.vonderbeck.bpm.identity.keycloak;
+
+import java.lang.reflect.Field;
+
+import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+
+/**
+ * Refresh access token test.
+ */
+public class KeycloakRefreshTokenTest extends KeycloakIdentityProviderTest {
+
+	/**
+	 * Tests refreshing access token.
+	 * @throws Exception in case of errors
+	 */
+	public void testRefreshToken() throws Exception {
+		// access Keycloak
+	    assertEquals(3, identityService.createUserQuery().count());
+
+	    // expire current token (the dirty way)
+	    KeycloakIdentityProviderFactory sessionFacory = (KeycloakIdentityProviderFactory) 
+	    		((ProcessEngineConfigurationImpl) processEngine.getProcessEngineConfiguration()).getIdentityProviderSessionFactory();
+	    KeycloakContext ctx = sessionFacory.keycloakContextProvider.context;
+	    Field expiresField = KeycloakContext.class.getDeclaredField("expiresAt");
+	    expiresField.setAccessible(true);
+	    expiresField.set(ctx, 0);
+	    assertTrue(ctx.needsRefresh());
+	    
+		// access Keycloak again
+	    assertEquals(3, identityService.createUserQuery().count());
+	    
+	}
+}
