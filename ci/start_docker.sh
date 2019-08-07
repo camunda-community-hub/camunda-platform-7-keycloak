dc06b73c2e4da098381b708388f2e9b4a6f52164
Christian Nicolai <christian.nicolai@camunda.com>
2019-08-07 11:41:02 +0200

    chore(ci): set used shell explicitly

--- ci/start_docker.sh
+++ ci/start_docker.sh
@@ -1,4 +1,7 @@
+#!/bin/sh
+
 docker run -d -e KEYCLOAK_USER=keycloak -e KEYCLOAK_PASSWORD=keycloak1! -e DB_VENDOR=h2 -p 8443:8443 -p 9001:8443 jboss/keycloak:6.0.1
+
 while ! wget --quiet --output-document=- --no-check-certificate https://localhost:8443/
 do
   echo "$(date) - still trying"
