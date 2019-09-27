a8aaba624f4f177af0097b8befba9176377cdcea
Gunnar von der Beck <Gunnar.von-der-Beck@accso.de>
2019-09-27 12:55:05 +0200

    chore: upgraded Keycloak test server to 7.0.0

--- ci/start_docker.sh
+++ ci/start_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run -d -e KEYCLOAK_USER=keycloak -e KEYCLOAK_PASSWORD=keycloak1! -e DB_VENDOR=h2 -p 8443:8443 -p 9001:8443 jboss/keycloak:6.0.1
+docker run -d -e KEYCLOAK_USER=keycloak -e KEYCLOAK_PASSWORD=keycloak1! -e DB_VENDOR=h2 -p 8443:8443 -p 9001:8443 jboss/keycloak:7.0.0
 
 while ! wget --quiet --output-document=- --no-check-certificate https://localhost:8443/
 do
