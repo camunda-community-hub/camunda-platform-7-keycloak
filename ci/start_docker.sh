805986f5ddd0b24ab9b937480c8b004c76f8241b
Gunnar von der Beck <Gunnar.von-der-Beck@accso.de>
2020-02-13 14:14:17 +0100

    chore: moved to Keycloak 8.0.2 as test server

--- ci/start_docker.sh
+++ ci/start_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run -d -e KEYCLOAK_USER=keycloak -e KEYCLOAK_PASSWORD=keycloak1! -e DB_VENDOR=h2 -p 8443:8443 -p 9001:8443 jboss/keycloak:7.0.0
+docker run -d -e KEYCLOAK_USER=keycloak -e KEYCLOAK_PASSWORD=keycloak1! -e DB_VENDOR=h2 -p 8443:8443 -p 9001:8443 jboss/keycloak:8.0.2
 
 while ! wget --quiet --output-document=- --no-check-certificate https://localhost:8443/
 do
