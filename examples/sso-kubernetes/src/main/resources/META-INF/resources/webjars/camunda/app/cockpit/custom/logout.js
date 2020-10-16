902d87d86987a72f5e5713a3fe549db31194da5d
Gunnar von der Beck <Gunnar.von-der-Beck@accso.de>
2020-10-16 17:58:21 +0200

    feat: Update to Camunda 7.14

--- examples/sso-kubernetes/src/main/resources/META-INF/resources/webjars/camunda/app/cockpit/custom/logout.js
+++ examples/sso-kubernetes/src/main/resources/META-INF/resources/webjars/camunda/app/cockpit/custom/logout.js
@@ -1,22 +1,10 @@
-'use strict';
-define('custom-logout', ['angular'], function (angular) {
-    var customLogoutModule = angular.module('custom-logout', []).run(
-        ['$rootScope', function ($rootScope) {
-
-            $rootScope.$on('$viewContentLoaded', function (event) {
-                // Get the HTML element of the header widget.
-                var div = document.querySelector("[cam-widget-header]");
-
-                // Get the only property on it, its key is jQuery<many numbers> and its value
-                // contains the controller ($camWidgetHeaderController) and the isolated
-                // Scope ($isolateScope). The logout function is defined in this scope.
-                var jQueryKey = Object.getOwnPropertyNames(div)[0];
-                var $isolateScope = div[jQueryKey]['$isolateScope'];
-                $isolateScope.logout = function () {
-                    // Do whatever you want to do on logout.
-                    window.location.href = "logout"
-                }
-
-            })
-        }]);
-});
+const doFetch = window.fetch;
+window.fetch = function() {
+    // Intercept calling logout
+    if (arguments[0] === '/camunda/api/admin/auth/user/default/logout') {
+      // do whatever you want to do on logout
+      window.location.href = "logout"
+    } else {
+      return doFetch.apply(this, arguments)
+    }
+}
